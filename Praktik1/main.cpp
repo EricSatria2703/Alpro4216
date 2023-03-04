03317e8a51b304af34ad166365286eadfc771feb
EricSatria2703 <111202214601@mhs.dinus.ac.id>
2023-03-04 18:56:25 +0700

    tugas praktik

---
+++ Praktik1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int mltpl(int x, int y, int z) {
+    return x*y*z;
+}
+
+void biodata(){
+    cout << "\nHalo nama saya Eric" << endl;
+    cout << "NIM saya A11.2022.14601" << endl;
+}
+
+string oddeven(int j) {
+    int k = j%2;
+    if (k!=0) {
+        return "ganjil";
+    }
+    else {
+        return "genap";
+    }
+
+}
+
+int main()
+{
+
+    cout << oddeven(6);
+
+    /*
+    string nama;
+    int usia;
+    string jurusan;
+    cout << "Biodata" << endl;
+    cout << "masukkan nama:" << endl;
+    cin >> nama;
+    cout << "masukkan usia:" << endl;
+    cin >> usia;
+    cout << "masukkan jurusan:" << endl;
+    cin >> jurusan;
+    cout << "Hallo nama saya " << nama << " umur saya " << usia << " saya kuliah di jurusan " << jurusan << endl;
+    if (usia%2 != 0) {
+        cout << "usia saya ganjil" << endl;
+    }
+    else {
+        cout << "usia saya genap" << endl;
+    }
+    */
+
+    /*cout << mltpl(1,2,3);
+    biodata();*/
+    return 0;
+}
